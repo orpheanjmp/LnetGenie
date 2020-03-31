53b1a8ae8408e519d40d384be7e1b998c6c918f7
Orphean <orpheanjmp@gmail.com>
2020-03-31 14:51:47 -0700

    Added basic connectivity to LNetwork

--- LNetwork/LNetwork.cs
+++ LNetwork/LNetwork.cs
@@ -1,152 +1,166 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Xml;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Net.Security;
-using System.Security.Authentication;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LNetwork
 {
+    internal class LnetContext
+    {
+        public byte[] Buffer;
+        public SslStream Stream;
+    }
+
     public class LNetwork
     {
-        private readonly string _host = "lnet.lichproject.org";
-        private readonly int _port = 7155;
-        private readonly string _pem = "-----BEGIN CERTIFICATE-----\nMIIDlTCCAn2gAwIBAgIJAKuu65i5NsruMA0GCSqGSIb3DQEBCwUAMGExCzAJBgNV\nBAYTAlVTMREwDwYDVQQIDAhJbGxpbm9pczESMBAGA1UECgwJTWF0dCBMb3dlMQ8w\nDQYDVQQDDAZSb290Q0ExGjAYBgkqhkiG9w0BCQEWC21hdHRAaW80LnVzMB4XDTE0\nMDYwNzE3NDUwMFoXDTI0MDYwNDE3NDUwMFowYTELMAkGA1UEBhMCVVMxETAPBgNV\nBAgMCElsbGlub2lzMRIwEAYDVQQKDAlNYXR0IExvd2UxDzANBgNVBAMMBlJvb3RD\nQTEaMBgGCSqGSIb3DQEJARYLbWF0dEBpbzQudXMwggEiMA0GCSqGSIb3DQEBAQUA\nA4IBDwAwggEKAoIBAQCcIRn0IMCNYeL5agKmkdedgJXsIyTJS8qKrY6EvQsq4tt0\nmO3Or9K8IaDl7qFdQ9nfSJ5phNgoCy9wZ9rDWv5FhY5MnnVHGr3fCa7RkMxJFR/N\nwiD4ihQlixOUly76glceyc/6QQS9bNe96evZDstERGAFfzgHY4qAlyurR6mBu9Mb\nyyCRok6xMRnjrbTMNkvvOsuG0sY9ot+SLHGgU3qT7+wVh/CbWcjeF7/Qwa//fbFk\nmq5c1FuvhU3DanSSz+VuWudPFSyZ3r5pYrLMJWsyomDa4gkL2bJ5jya2BWDMXvSS\nCpdQgPDIlClMfAFLd/Ss8ZIGa6uNFcSK6Xca51ClAgMBAAGjUDBOMB0GA1UdDgQW\nBBScbglRiGzz9yzuhgBwFYjgimeByDAfBgNVHSMEGDAWgBScbglRiGzz9yzuhgBw\nFYjgimeByDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQA7MLZYfqam\n5aaSBqQpT6sOGDtVc9koIok59oTQmNXqe+awg2VUnAiesxtLd+FWGUMp8XzHdGWw\nH3O6kAUkPm/in001X7TRAhbgDujfTRbTzxND0XrjuEzDMALs3YpDM1pMXqC7RXWA\n7z+N0gRaUgmh1rMbk/qA3cAfC2dwf2j3NYy3bDw3lMpdyIwAfOQxiZVglYgX3dgT\nU9b//gsUyPCvlpL0mYcmhOOLt6oqQhMJaw1I6A9xMe2kO2L+8KPGK2u1B+P5/Sx0\nFE8LIp5KA3a7yRbOty19NsGR+yW7WwV7BL6c6GOKb/iKJBLYzTmNG6m16hRrxDGj\ntGu91I0ORptB\n-----END CERTIFICATE-----";
-        
-        private X509Certificate2 _cert;
-        private X509Store _store;
+        private const string Host = "lnet.lichproject.org";
+        private const int Port = 7155;
+
+        private const string Pem =
+            "-----BEGIN CERTIFICATE-----\nMIIDlTCCAn2gAwIBAgIJAKuu65i5NsruMA0GCSqGSIb3DQEBCwUAMGExCzAJBgNV\nBAYTAlVTMREwDwYDVQQIDAhJbGxpbm9pczESMBAGA1UECgwJTWF0dCBMb3dlMQ8w\nDQYDVQQDDAZSb290Q0ExGjAYBgkqhkiG9w0BCQEWC21hdHRAaW80LnVzMB4XDTE0\nMDYwNzE3NDUwMFoXDTI0MDYwNDE3NDUwMFowYTELMAkGA1UEBhMCVVMxETAPBgNV\nBAgMCElsbGlub2lzMRIwEAYDVQQKDAlNYXR0IExvd2UxDzANBgNVBAMMBlJvb3RD\nQTEaMBgGCSqGSIb3DQEJARYLbWF0dEBpbzQudXMwggEiMA0GCSqGSIb3DQEBAQUA\nA4IBDwAwggEKAoIBAQCcIRn0IMCNYeL5agKmkdedgJXsIyTJS8qKrY6EvQsq4tt0\nmO3Or9K8IaDl7qFdQ9nfSJ5phNgoCy9wZ9rDWv5FhY5MnnVHGr3fCa7RkMxJFR/N\nwiD4ihQlixOUly76glceyc/6QQS9bNe96evZDstERGAFfzgHY4qAlyurR6mBu9Mb\nyyCRok6xMRnjrbTMNkvvOsuG0sY9ot+SLHGgU3qT7+wVh/CbWcjeF7/Qwa//fbFk\nmq5c1FuvhU3DanSSz+VuWudPFSyZ3r5pYrLMJWsyomDa4gkL2bJ5jya2BWDMXvSS\nCpdQgPDIlClMfAFLd/Ss8ZIGa6uNFcSK6Xca51ClAgMBAAGjUDBOMB0GA1UdDgQW\nBBScbglRiGzz9yzuhgBwFYjgimeByDAfBgNVHSMEGDAWgBScbglRiGzz9yzuhgBw\nFYjgimeByDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQA7MLZYfqam\n5aaSBqQpT6sOGDtVc9koIok59oTQmNXqe+awg2VUnAiesxtLd+FWGUMp8XzHdGWw\nH3O6kAUkPm/in001X7TRAhbgDujfTRbTzxND0XrjuEzDMALs3YpDM1pMXqC7RXWA\n7z+N0gRaUgmh1rMbk/qA3cAfC2dwf2j3NYy3bDw3lMpdyIwAfOQxiZVglYgX3dgT\nU9b//gsUyPCvlpL0mYcmhOOLt6oqQhMJaw1I6A9xMe2kO2L+8KPGK2u1B+P5/Sx0\nFE8LIp5KA3a7yRbOty19NsGR+yW7WwV7BL6c6GOKb/iKJBLYzTmNG6m16hRrxDGj\ntGu91I0ORptB\n-----END CERTIFICATE-----";
+
+
         private TcpClient _client;
+        private readonly LnetContext _context = new LnetContext();
+        private bool reading = false;
 
         public LNetwork()
         {
-            _cert = new X509Certificate2();
-            _cert.Import(Encoding.ASCII.GetBytes(_pem));
+            var cert = new X509Certificate2();
+            cert.Import(Encoding.ASCII.GetBytes(Pem));
 
-            _store = new X509Store("lnetgenie");
-            _store.Open(OpenFlags.MaxAllowed);
-            _store.Add(_cert);
-            _store.Close();
+            var store = new X509Store("lnetgenie");
+            store.Open(OpenFlags.MaxAllowed);
+            store.Add(cert);
+            store.Close();
+
+            _context.Buffer = new byte[32768];
         }
 
         public void Test()
         {
-            Console.WriteLine("Hello World");
             Connect();
+
+            Thread.CurrentThread.Name = "Lnet";
+
+            var connection = Task.Factory.StartNew(() => {
+                while (true)
+                {
+                    if (!_context.Stream.CanRead)
+                    {
+                        Console.WriteLine("Can't Read!");
+                        Disconnect();
+                        Connect();
+                    }
+
+                    if (!reading)
+                    {
+                        _context.Stream.BeginRead( _context.Buffer, 0,  _context.Buffer.Length, GetMessage, _context);
+                        reading = true;
+                    }
+                } });
+
+            Login();
         }
 
-       
-        public void Connect()
-        {
-            _client = new TcpClient(_host, _port);
-            Console.WriteLine("Connected");
 
-            SslStream sslStream = new SslStream(
+        private void GetMessage(IAsyncResult result)
+        {
+            var context = (LnetContext) result.AsyncState;
+
+            var messageData = new StringBuilder();
+
+            var size = context.Stream.EndRead(result);
+
+            var decoder = Encoding.UTF8.GetDecoder();
+            var chars = new char[decoder.GetCharCount(context.Buffer, 0, size)];
+            decoder.GetChars(context.Buffer, 0, size, chars, 0);
+            messageData.Append(chars);
+
+            var message = messageData.ToString();
+
+            Console.WriteLine(message);
+
+
+            var ping = new Regex("^<ping/>$");
+
+            if (ping.IsMatch(message))
+            {
+                Pong();
+            }
+
+            reading = false;
+        }
+
+        private void Connect()
+        {
+            _client = new TcpClient(Host, Port);
+            Console.WriteLine("Connected to Lnet");
+
+            _context.Stream = new SslStream(
                 _client.GetStream(),
                 false,
-                new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                ValidateServerCertificate,
                 null);
 
-
-            try 
+            try
             {
-                sslStream.AuthenticateAsClient("lichproject.org");
-            } 
+                _context.Stream.AuthenticateAsClient(Host);
+            }
             catch (AuthenticationException e)
             {
                 Console.WriteLine("Exception: {0}", e.Message);
-                if (e.InnerException != null)
-                {
-                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
-                }
-                Console.WriteLine ("Authentication failed - closing the connection.");
+                if (e.InnerException != null) Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+
+                Console.WriteLine("Authentication failed - closing the connection.");
                 _client.Close();
-                return;
             }
 
-            XmlDocument login = new XmlDocument();
+        }
 
-            login.LoadXml("<login name=\"Nery\" game=\"DR\" client=\"1.6\" lich=\"4.3.12\"></login>");
+        private void Disconnect()
+        {
+            if (!_client.Connected) return;
+            _client.Close();
+            Console.WriteLine("Disconnected from Lnet");
+        }
 
-            XmlDeclaration xmldecl;
-            xmldecl = login.CreateXmlDeclaration("1.0", null, null);
-            xmldecl.Encoding = "UTF-8";
-            xmldecl.Standalone = "yes";
+        private static bool ValidateServerCertificate(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
 
-            XmlElement root = login.DocumentElement;
-            login.InsertBefore(xmldecl, root);
+        private void Login()
+        {
+            const string login = "<login name='Nery' game='DR' client='1.6' lich='4.7'></login>";
 
+            if (!_context.Stream.CanWrite) return;
 
             Console.WriteLine(login);
-
-            StringWriter sw = new StringWriter();
-            XmlWriter xmlr =XmlWriter.Create(sw);
-
-            login.WriteTo(xmlr);
-            xmlr.Flush();
-
-            Console.WriteLine(sw.GetStringBuilder().ToString());
-
-            sslStream.Write(Encoding.ASCII.GetBytes(sw.GetStringBuilder().ToString()));
-            sslStream.Flush();
-            
-            ReadMessage(sslStream);
-            ReadMessage(sslStream);
-            ReadMessage(sslStream);
-            _client.Close();
+            _context.Stream.Write(Encoding.UTF8.GetBytes(login));
+            _context.Stream.Flush();
         }
 
-        string ReadMessage(SslStream sslStream)
+        private void Pong()
         {
-            Console.WriteLine("Starting ReadMessage");
-            // Read the  message sent by the server.
-            // The end of the message is signaled using the
-            // "<EOF>" marker.
-            byte [] buffer = new byte[2048];
-            StringBuilder messageData = new StringBuilder();
-            int bytes = -1;
-            do
-            {
-                Console.WriteLine("Pre read");
-                bytes = sslStream.Read(buffer, 0, buffer.Length);
-                Console.WriteLine("Post Read");
-                        
-                // Use Decoder class to convert from bytes to UTF8
-                // in case a character spans two buffers.
-                Decoder decoder = Encoding.UTF8.GetDecoder();
-                char[] chars = new char[decoder.GetCharCount(buffer,0,bytes)];
-                decoder.GetChars(buffer, 0, bytes, chars,0);
-                messageData.Append (chars);
-                // Check for EOF.
-                if (messageData.ToString().IndexOf("<EOF>") != -1)
-                {
-                    break;
-                }
-            } while (bytes != 0); 
-            
+            const string pong = "<pong/>";
 
-            Console.WriteLine("Read Message:");
-            Console.WriteLine(messageData.ToString());
-            return messageData.ToString();
-        }
+            if (!_context.Stream.CanWrite) return;
 
-        public static bool ValidateServerCertificate(
-              object sender,
-              X509Certificate certificate,
-              X509Chain chain,
-              SslPolicyErrors sslPolicyErrors)
-        {
-           if (sslPolicyErrors == SslPolicyErrors.None)
-                return true;
-
-            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
-            
-            // Do not allow this client to communicate with unauthenticated servers.
-            return true;
+            Console.WriteLine(pong);
+            _context.Stream.Write(Encoding.UTF8.GetBytes(pong));
+            _context.Stream.Flush();
         }
     }
 }
